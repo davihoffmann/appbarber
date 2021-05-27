361269b4df8b8cacc843fbcdee63f970abec0c61
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-05-27 16:46:59 -0300

    teste em configuração do eslint

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import SignIn from '../pages/SignIn'
-import SignUp from '../pages/SignUp'
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 
 const Auth = createStackNavigator();
 
 const AuthRoutes: React.FC = () => (
-    <Auth.Navigator screenOptions={{
-        headerShown: false,
-        // headerTintColor: '#fff',
-        // headerStyle: {
-        //     backgroundColor: '#c3c3c3',
-        // },
-        cardStyle: { backgroundColor: '#312e38' }
-    }}>
+    <Auth.Navigator
+        screenOptions={{
+            headerShown: false,
+            // headerTintColor: '#fff',
+            // headerStyle: {
+            //     backgroundColor: '#c3c3c3',
+            // },
+            cardStyle: { backgroundColor: '#312e38' },
+        }}
+    >
         <Auth.Screen name="SignIn" component={SignIn} />
         <Auth.Screen name="SignUp" component={SignUp} />
     </Auth.Navigator>
