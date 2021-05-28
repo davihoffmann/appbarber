db43eaa889876246e3a9bc8638dc5a73d68f5899
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-05-27 21:11:01 -0300

    criado componente de esqueceu a senha e para fazer cadastro

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,29 +1,47 @@
 import React from 'react';
 import { Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+    Container,
+    Title,
+    ForgotPassword,
+    ForgotPasswordText,
+    CreateAccountButton,
+    CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => (
-    <Container>
-        <Image source={logoImg} />
-        <Title>Faça seu logon</Title>
+    <>
+        <Container>
+            <Image source={logoImg} />
+            <Title>Faça seu logon</Title>
 
-        <Input name="email" icon="mail" placeholder="E-mail" />
-        <Input name="password" icon="lock" placeholder="Senha" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
 
-        <Button
-            onPress={() => {
-                console.log('deu');
-            }}
-        >
-            Entrar
-        </Button>
-    </Container>
+            <Button
+                onPress={() => {
+                    console.log('deu');
+                }}
+            >
+                Entrar
+            </Button>
+
+            <ForgotPassword>
+                <ForgotPasswordText>Esqueci minha Senha</ForgotPasswordText>
+            </ForgotPassword>
+        </Container>
+        <CreateAccountButton>
+            <Icon name="log-in" size={20} color="#ff9000" />
+            <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+        </CreateAccountButton>
+    </>
 );
 
 export default SignIn;
