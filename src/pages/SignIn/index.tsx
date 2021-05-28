c723d1592903df18396fc6cdba5f5a0c606b6348
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-05-27 21:18:39 -0300

    Desenvolvimento para tela de login ajustar com o keyboard ativo e ter scroll caso necessário

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
@@ -18,25 +18,31 @@ import {
 
 const SignIn: React.FC = () => (
     <>
-        <Container>
-            <Image source={logoImg} />
-            <Title>Faça seu logon</Title>
+        <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : undefined} enabled>
+            <ScrollView contentContainerStyle={{ flex: 1 }} keyboardShouldPersistTaps="handled">
+                <Container>
+                    <Image source={logoImg} />
+                    <View>
+                        <Title>Faça seu logon</Title>
+                    </View>
 
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="password" icon="lock" placeholder="Senha" />
+                    <Input name="email" icon="mail" placeholder="E-mail" />
+                    <Input name="password" icon="lock" placeholder="Senha" />
 
-            <Button
-                onPress={() => {
-                    console.log('deu');
-                }}
-            >
-                Entrar
-            </Button>
+                    <Button
+                        onPress={() => {
+                            console.log('deu');
+                        }}
+                    >
+                        Entrar
+                    </Button>
 
-            <ForgotPassword>
-                <ForgotPasswordText>Esqueci minha Senha</ForgotPasswordText>
-            </ForgotPassword>
-        </Container>
+                    <ForgotPassword>
+                        <ForgotPasswordText>Esqueci minha Senha</ForgotPasswordText>
+                    </ForgotPassword>
+                </Container>
+            </ScrollView>
+        </KeyboardAvoidingView>
         <CreateAccountButton>
             <Icon name="log-in" size={20} color="#ff9000" />
             <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
