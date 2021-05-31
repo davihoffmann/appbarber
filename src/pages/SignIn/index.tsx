2614f182859e7821d111564868e7819020ae461c
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-05-31 20:59:28 -0300

    Desenvolvimento da tela de cadastro de usuario e navegação entre tela de login e cadastro

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -16,38 +17,42 @@ import {
     CreateAccountButtonText,
 } from './styles';
 
-const SignIn: React.FC = () => (
-    <>
-        <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : undefined} enabled>
-            <ScrollView contentContainerStyle={{ flex: 1 }} keyboardShouldPersistTaps="handled">
-                <Container>
-                    <Image source={logoImg} />
-                    <View>
-                        <Title>Faça seu logon</Title>
-                    </View>
+const SignIn: React.FC = () => {
+    const navigation = useNavigation();
 
-                    <Input name="email" icon="mail" placeholder="E-mail" />
-                    <Input name="password" icon="lock" placeholder="Senha" />
+    return (
+        <>
+            <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : undefined} enabled>
+                <ScrollView contentContainerStyle={{ flex: 1 }} keyboardShouldPersistTaps="handled">
+                    <Container>
+                        <Image source={logoImg} />
+                        <View>
+                            <Title>Faça seu logon</Title>
+                        </View>
 
-                    <Button
-                        onPress={() => {
-                            console.log('deu');
-                        }}
-                    >
-                        Entrar
-                    </Button>
+                        <Input name="email" icon="mail" placeholder="E-mail" />
+                        <Input name="password" icon="lock" placeholder="Senha" />
 
-                    <ForgotPassword>
-                        <ForgotPasswordText>Esqueci minha Senha</ForgotPasswordText>
-                    </ForgotPassword>
-                </Container>
-            </ScrollView>
-        </KeyboardAvoidingView>
-        <CreateAccountButton>
-            <Icon name="log-in" size={20} color="#ff9000" />
-            <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
-        </CreateAccountButton>
-    </>
-);
+                        <Button
+                            onPress={() => {
+                                console.log('deu');
+                            }}
+                        >
+                            Entrar
+                        </Button>
+
+                        <ForgotPassword>
+                            <ForgotPasswordText>Esqueci minha Senha</ForgotPasswordText>
+                        </ForgotPassword>
+                    </Container>
+                </ScrollView>
+            </KeyboardAvoidingView>
+            <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
+                <Icon name="log-in" size={20} color="#ff9000" />
+                <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+            </CreateAccountButton>
+        </>
+    );
+};
 
 export default SignIn;
