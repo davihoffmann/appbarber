50aac6db0fe367ac244f164327d62bcad5f375bc
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-05-31 21:50:32 -0300

    Iniciado criação do funcionamento do formulario

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
-
+import React, { useEffect, useRef } from 'react';
+import { useField } from '@unform/core';
 import { InputProps } from './types';
 import { Container, TextInput, Icon } from './styles';
 
-const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => (
-    <Container>
-        <Icon name={icon} size={20} color="#666360" />
-        <TextInput {...rest} placeholderTextColor="#666360" keyboardAppearance="dark" />
-    </Container>
-);
+interface InputValueReference {
+    value: string;
+}
+
+const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
+    const { registerField, defaultValue = '', fieldName, error } = useField(name);
+    const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
+
+    useEffect(() => {
+        registerField({
+            name: fieldName,
+            ref: inputValueRef.current,
+            path: 'value',
+        });
+    }, [fieldName, registerField]);
+
+    return (
+        <Container>
+            <Icon name={icon} size={20} color="#666360" />
+            <TextInput
+                {...rest}
+                placeholderTextColor="#666360"
+                keyboardAppearance="dark"
+                defaultValue={defaultValue}
+                onChangeText={value => {
+                    inputValueRef.current.value = value;
+                }}
+            />
+        </Container>
+    );
+};
 
 export default Input;
