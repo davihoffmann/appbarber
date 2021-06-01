1c3f6b0552390b297f6bde06db698ca729d83700
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-06-01 16:54:08 -0300

    Ajuste de UI na tela de cadastro

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useImperativeHandle, forwardRef } from 'react';
 import { useField } from '@unform/core';
 import { InputProps } from './types';
 import { Container, TextInput, Icon } from './styles';
@@ -7,18 +7,28 @@ interface InputValueReference {
     value: string;
 }
 
-const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
+interface InputRef {
+    focus(): void;
+}
+
+const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = ({ name, icon, ...rest }, ref) => {
     const inputElementRef = useRef<any>(null);
 
     const { registerField, defaultValue = '', fieldName, error } = useField(name);
     const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
+
+    useImperativeHandle(ref, () => ({
+        focus() {
+            inputElementRef.current.focus();
+        },
+    }));
 
     useEffect(() => {
         registerField<string>({
             name: fieldName,
             ref: inputValueRef.current,
             path: 'value',
-            setValue(ref: any, value: string) {
+            setValue(reference: any, value: string) {
                 inputValueRef.current.value = value;
                 inputElementRef.current.setNativeProps({ text: value });
             },
@@ -46,4 +56,4 @@ const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
     );
 };
 
-export default Input;
+export default forwardRef(Input);
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useRef } from 'react';
-import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
@@ -21,6 +21,7 @@ import {
 
 const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
+    const passwordInputRef = useRef<TextInput>(null);
     const navigation = useNavigation();
 
     const handleSignIn = useCallback((data: any) => {
@@ -38,8 +39,29 @@ const SignIn: React.FC = () => {
                         </View>
 
                         <Form onSubmit={handleSignIn} ref={formRef} style={{ width: '100%' }}>
-                            <Input name="email" icon="mail" placeholder="E-mail" />
-                            <Input name="password" icon="lock" placeholder="Senha" />
+                            <Input
+                                autoCorrect={false}
+                                autoCapitalize="none"
+                                keyboardType="email-address"
+                                name="email"
+                                icon="mail"
+                                placeholder="E-mail"
+                                returnKeyType="next"
+                                onSubmitEditing={() => {
+                                    passwordInputRef.current?.focus();
+                                }}
+                            />
+                            <Input
+                                ref={passwordInputRef}
+                                name="password"
+                                icon="lock"
+                                placeholder="Senha"
+                                secureTextEntry
+                                returnKeyType="send"
+                                onSubmitEditing={() => {
+                                    formRef.current?.submitForm();
+                                }}
+                            />
 
                             <Button
                                 onPress={() => {
