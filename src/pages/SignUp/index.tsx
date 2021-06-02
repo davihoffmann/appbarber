cc48c83911f712589a6491a0ff16c1238398e1b9
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-06-02 17:13:27 -0300

    Ajustado dependencia de função de cadastro de usuário

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -29,34 +29,37 @@ const SignUp: React.FC = () => {
 
     const navigation = useNavigation();
 
-    const handleSubmit = useCallback(async (data: SignUpFormData) => {
-        try {
-            formRef.current?.setErrors({});
+    const handleSubmit = useCallback(
+        async (data: SignUpFormData) => {
+            try {
+                formRef.current?.setErrors({});
 
-            const schema = Yup.object().shape({
-                name: Yup.string().required('Nome Obrigatório'),
-                email: Yup.string().required('E-mail Obrigatório').email('Digite um e-mail válido'),
-                password: Yup.string().required('Senha Obrigatória'),
-            });
+                const schema = Yup.object().shape({
+                    name: Yup.string().required('Nome Obrigatório'),
+                    email: Yup.string().required('E-mail Obrigatório').email('Digite um e-mail válido'),
+                    password: Yup.string().required('Senha Obrigatória'),
+                });
 
-            await schema.validate(data, {
-                abortEarly: false,
-            });
+                await schema.validate(data, {
+                    abortEarly: false,
+                });
 
-            await api.post('/users', data);
+                await api.post('/users', data);
 
-            Alert.alert('Cadastro realizado com sucesso!', 'Você já pode fazer login na aplicação!');
+                Alert.alert('Cadastro realizado com sucesso!', 'Você já pode fazer login na aplicação!');
 
-            navigation.goBack();
-        } catch (err) {
-            if (err instanceof Yup.ValidationError) {
-                const errors = getValidationErrors(err);
-                formRef.current?.setErrors(errors);
-                return;
+                navigation.goBack();
+            } catch (err) {
+                if (err instanceof Yup.ValidationError) {
+                    const errors = getValidationErrors(err);
+                    formRef.current?.setErrors(errors);
+                    return;
+                }
+                Alert.alert('Erro na autenticação', 'Ocorreu um erro ao fazer login, cheque suas credênciais');
             }
-            Alert.alert('Erro na autenticação', 'Ocorreu um erro ao fazer login, cheque suas credênciais');
-        }
-    }, []);
+        },
+        [navigation]
+    );
 
     return (
         <>
