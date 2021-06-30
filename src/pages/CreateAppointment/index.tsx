8c305db731b0a1d5c46afbd0c0e6043dab1ecb1e
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-06-30 08:15:13 -0300

    Desenvolvido header do create appointment page

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,6 +1,36 @@
-import React, { ReactElement } from 'react';
-import { View } from 'react-native';
+import React, { ReactElement, useCallback } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useAuth } from '../../hooks/auth';
+
+import { Container, Header, BackButton, HeaderTitle, UserAvatar } from './styles';
+
+interface RouteParams {
+    providerId: string;
+}
 
 export default function CreateAppointment(): ReactElement {
-    return <View />;
+    const { user } = useAuth();
+    const { goBack } = useNavigation();
+    const route = useRoute();
+    const { providerId } = route.params as RouteParams;
+
+    const naviteBack = useCallback(() => {
+        goBack();
+    }, [goBack]);
+
+    return (
+        <Container>
+            <Header>
+                <BackButton onPress={naviteBack}>
+                    <Icon name="chevron-left" size={24} color="#999591" />
+                </BackButton>
+
+                <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+                <UserAvatar source={{ uri: user.avatar_url }} />
+            </Header>
+        </Container>
+    );
 }
