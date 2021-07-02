6fced56f2943b1e191cb5e43716acbe353408ba5
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-07-02 18:12:44 -0300

    Desenvolvida tela de sucesso de agendamento

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,6 +1,33 @@
-import React, { ReactElement } from 'react';
-import { View } from 'react-native';
+import React, { ReactElement, useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+import { Container, Title, Description, OkButton, OkButtonText } from './styles';
 
 export default function AppointmentCreated(): ReactElement {
-    return <View />;
+    const { reset } = useNavigation();
+
+    const handleOkPressed = useCallback(() => {
+        reset({
+            routes: [
+                {
+                    name: 'Dashboard',
+                },
+            ],
+            index: 0,
+        });
+    }, [reset]);
+
+    return (
+        <Container>
+            <Icon name="check" size={80} color="#04d361" />
+
+            <Title>Agendamento Concluido</Title>
+
+            <Description>terça, dia 14 de março de 2020 às 12:00</Description>
+
+            <OkButton onPress={handleOkPressed}>
+                <OkButtonText>ok</OkButtonText>
+            </OkButton>
+        </Container>
+    );
 }
