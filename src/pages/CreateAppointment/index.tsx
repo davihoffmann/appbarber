93bc47c7b1dc8ca818b9ea0b17bcba15acca8adf
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-07-02 17:56:50 -0300

    Finalizada cadastro de agendamentos

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,13 +1,11 @@
 import React, { ReactElement, useCallback, useEffect, useState, useMemo } from 'react';
-import { Platform } from 'react-native';
+import { Platform, Alert } from 'react-native';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { format } from 'date-fns';
-
 import api from '../../services/api';
 import { useAuth } from '../../hooks/auth';
-
 import { Provider } from '../Dashboard/types';
 import {
     Container,
@@ -31,6 +29,8 @@ import {
     SectionContent,
     Hour,
     HourText,
+    CreateAppointmentButton,
+    CreateAppointmentBottonText,
 } from './styles';
 
 interface RouteParams {
@@ -44,7 +44,7 @@ interface AvailabilityItem {
 
 export default function CreateAppointment(): ReactElement {
     const { user } = useAuth();
-    const { goBack } = useNavigation();
+    const { goBack, navigate } = useNavigation();
     const route = useRoute();
     const routeParams = route.params as RouteParams;
 
@@ -98,6 +98,27 @@ export default function CreateAppointment(): ReactElement {
     const handleSelectHour = useCallback((hour: number) => {
         setSelectedHour(hour);
     }, []);
+
+    const handleCreateAppointment = useCallback(async () => {
+        try {
+            const date = new Date(selectedDate);
+
+            date.setHours(selectedHour);
+            date.setMinutes(0);
+
+            await api.post('appointments', {
+                provider_id: selectedProvider,
+                date,
+            });
+
+            navigate('AppointmentCreated', { date: date.getTime() });
+        } catch (err) {
+            Alert.alert(
+                'Erro ao criar agendamento!',
+                'Ocorreu um erro ao tentar criar o agendamento, tente novamente!'
+            );
+        }
+    }, [navigate, selectedDate, selectedHour, selectedProvider]);
 
     const morningAvailability = useMemo(() => {
         return availability
@@ -211,6 +232,10 @@ export default function CreateAppointment(): ReactElement {
                         </SectionContent>
                     </Section>
                 </Schedule>
+
+                <CreateAppointmentButton onPress={handleCreateAppointment}>
+                    <CreateAppointmentBottonText>Agendar</CreateAppointmentBottonText>
+                </CreateAppointmentButton>
             </Content>
         </Container>
     );
